fb217aafc3c89df21b02f844e705df123ec93bc8
xzy <2356825441@qq.com>
2017-06-13 10:14:07 +0800

    第二次更改http地址

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "xuezy.gg.po",
   "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
